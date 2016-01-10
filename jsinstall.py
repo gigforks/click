cfe624471506df92c2e4c28d90583bf82c9665ed
kristof de spiegeleer <kristof@incubaid.com>
2016-01-10 10:05:23 +0400

    new

---
+++ jsinstall.py
@@ -0,0 +1,8 @@
+from JumpScale import j
+
+j.do.execute("python3 setup.py build")
+
+j.do.execute("rm -rf %s/click/*"%j.do.getPythonLibSystem())
+
+j.do.copyTree("build/lib/", dest=j.do.getPythonLibSystem(), keepsymlinks = False, deletefirst = False, overwriteFiles=True,rsync=False,recursive=True)
+
